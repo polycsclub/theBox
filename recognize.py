ff0829e5726700b3762da6ee5db960cee4791146
Ed Green <greene2@sunyit.edu>
2018-12-05 02:03:43 -0500

    Fixed LED blinking issue

--- recognize.py
+++ recognize.py
@@ -34,14 +34,13 @@ while True :
 
     with open("Last.jpg", "rb") as f :
         rekognition_result = rekognition.detect_labels(Image={"Bytes":f.read()}, MaxLabels=16, MinConfidence=70)	# Submit Rekognition request
-
+    stat = False
     # Parse Rekognition result
     for instance in rekognition_result["Labels"]:
         print(str(instance["Name"]) + " : " + str(instance["Confidence"]))
         if str(instance["Name"]) == object and instance["Confidence"] >= threshold :
             print("Valid " + object);
-            GPIO.output(26, GPIO.LOW)
-        else:
-            GPIO.output(26, GPIO.HIGH)
+            stat = True
+    GPIO.output(26, stat)
 
 GPIO.cleanup([26, 20, 21])

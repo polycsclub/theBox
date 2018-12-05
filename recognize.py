cabd48ce03945d257ef1bb840ac35f946f60901d
Ed Green <greene2@sunyit.edu>
2018-12-05 07:10:12 +0000

    fixed inverted status

--- recognize.py
+++ recognize.py
@@ -34,13 +34,13 @@ while True :
 
     with open("Last.jpg", "rb") as f :
         rekognition_result = rekognition.detect_labels(Image={"Bytes":f.read()}, MaxLabels=16, MinConfidence=70)	# Submit Rekognition request
-    stat = False
+    stat = True
     # Parse Rekognition result
     for instance in rekognition_result["Labels"]:
         print(str(instance["Name"]) + " : " + str(instance["Confidence"]))
         if str(instance["Name"]) == object and instance["Confidence"] >= threshold :
             print("Valid " + object);
-            stat = True
+            stat = False
     GPIO.output(26, stat)
 
 GPIO.cleanup([26, 20, 21])
